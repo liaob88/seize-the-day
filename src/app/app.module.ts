cce8c9891839d13fae66b8c404eac071c82cd002
ryotaro-kitamura <k.ryoutaro.g@gmail.com>
2020-02-19 07:51:15 +0900

    yarn ng g c components/item-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { ItemComponent } from "./components/item/item.component";
+import { ItemListComponent } from './components/item-list/item-list.component';
 
 @NgModule({
-  declarations: [AppComponent, ItemComponent],
+  declarations: [AppComponent, ItemComponent, ItemListComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
