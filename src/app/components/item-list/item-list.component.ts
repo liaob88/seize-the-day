cecdb7ea8f561c08e7add3eedbfb5f836b68df44
ryotaro-kitamura <k.ryoutaro.g@gmail.com>
2020-02-19 07:59:52 +0900

    item-list component から item component にデータを渡して表示させた

--- src/app/components/item-list/item-list.component.ts
+++ src/app/components/item-list/item-list.component.ts
@@ -1,3 +1,4 @@
+import { Item } from "./../../shared/models";
 import { Component, OnInit } from "@angular/core";
 
 @Component({
@@ -6,6 +7,24 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./item-list.component.sass"]
 })
 export class ItemListComponent implements OnInit {
+  items: Item[] = [
+    {
+      title: "Test 1",
+      createdAt: "2019-01-01"
+    },
+    {
+      title: "Test 2",
+      createdAt: "2019-01-02"
+    },
+    {
+      title: "Test 3",
+      createdAt: "2019-01-03"
+    },
+    {
+      title: "Test 4",
+      createdAt: "2019-01-04"
+    }
+  ];
   constructor() {}
 
   ngOnInit() {}
