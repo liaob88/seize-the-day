31e96460c9e043e150d86610758fbd684a2c7eec
ryotaro-kitamura <k.ryoutaro.g@gmail.com>
2020-02-24 12:09:32 +0900

    routing 再設定

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import { ItemEditComponent } from "./components/item-list/components/item-edit/item-edit.component";
+import { ItemListComponent } from "./components/item-list/item-list.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "list", pathMatch: "full" },
+  { path: "list", component: ItemListComponent },
+  { path: "edit/:id", component: ItemEditComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
