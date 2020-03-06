8ebe00edd7f391ad1d1a82b6fee5f92a58fc17d3
ryotaro-kitamura <k.ryoutaro.g@gmail.com>
2020-03-06 23:56:00 +0900

    header の作成までを実装

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,13 @@ import { StoreModule } from "@ngrx/store";
 import { StoreDevtoolsModule } from "@ngrx/store-devtools";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { ItemAddComponent } from "./components/item-list/components/item-add/item-add.component";
 import { ItemEditComponent } from "./components/item-list/components/item-edit/item-edit.component";
 import { ItemComponent } from "./components/item-list/components/item/item.component";
 import { ItemListComponent } from "./components/item-list/item-list.component";
 import { ItemListService } from "./components/item-list/item-list.service";
 import reducer from "./store/store";
-import { ItemAddComponent } from "./components/item-list/components/item-add/item-add.component";
+import { MaterialModule } from "./styles/material.module";
 
 @NgModule({
   declarations: [
@@ -24,6 +25,7 @@ import { ItemAddComponent } from "./components/item-list/components/item-add/ite
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    MaterialModule,
     StoreModule.forRoot({ itemList: reducer }),
     StoreDevtoolsModule.instrument()
   ],
