61a2bb9c8834788f76fe845a136fbe77e7798092
ryotaro-kitamura <k.ryoutaro.g@gmail.com>
2020-03-14 22:13:03 +0900

    editor と preview 機能の追加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
-import { ItemAddComponent } from "./pages/item-add/item-add.component";
-import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
-import { BrowserModule } from "@angular/platform-browser";
-import { StoreModule } from "@ngrx/store";
-import { StoreDevtoolsModule } from "@ngrx/store-devtools";
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { ItemEditComponent } from "./pages/item-edit/item-edit.component";
-import { ItemComponent } from "./components/item/item.component";
-import { ItemListComponent } from "./pages/item-list/item-list.component";
-import { ItemListService } from "./pages/item-list/item-list.service";
-import reducer from "./store/store";
-import { MaterialModule } from "./shared/styles/material.module";
-import { ButtonComponent } from "./shared/components/button/button.component";
+import { ItemAddComponent } from './pages/item-add/item-add.component';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ItemEditComponent } from './pages/item-edit/item-edit.component';
+import { ItemComponent } from './components/item/item.component';
+import { ItemListComponent } from './pages/item-list/item-list.component';
+import { ItemListService } from './pages/item-list/item-list.service';
+import reducer from './store/store';
+import { MaterialModule } from './shared/styles/material.module';
+import { ButtonComponent } from './shared/components/button/button.component';
 import { MarkdownPipe } from './shared/pipes/markdown.pipe';
 
 @NgModule({
--- src/app/shared/models.ts
+++ src/app/shared/models.ts
@@ -1,11 +1,13 @@
 export class Item {
   id: number;
   title: string;
+  contents: string;
   createdAt: Date;
 
-  constructor(id: number, title: string, createdAt: Date) {
+  constructor(id: number, title: string, contents: string, createdAt: Date) {
     this.id = id;
     this.title = title;
+    this.contents = contents;
     this.createdAt = createdAt;
   }
 }
