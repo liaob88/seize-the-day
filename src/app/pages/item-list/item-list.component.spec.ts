8cd1c2740e6d7701c2a0a5e9778214b83a32ae15
ryotaro-kitamura <k.ryoutaro.g@gmail.com>
2020-03-22 11:58:08 +0900

    import 整理と不要なコード削除

--- src/app/pages/item-list/item-list.component.spec.ts
+++ src/app/pages/item-list/item-list.component.spec.ts
@@ -1,34 +1,16 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core';
-import { ItemCreateComponent } from '../item-create/item-create.component';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormsModule } from '@angular/forms';
-import { RouterTestingModule } from '@angular/router/testing';
-import { StoreModule } from '@ngrx/store';
-import { provideMockStore } from '@ngrx/store/testing';
-import { ItemCardComponent } from '../../components/item-card/item-card.component';
 import { ItemListComponent } from './item-list.component';
-import { ItemListService } from './item-list.service';
-import { MarkdownPipe } from 'src/app/shared/pipes/markdown.pipe';
 
 describe('ItemListComponent', () => {
   let component: ItemListComponent;
   let fixture: ComponentFixture<ItemListComponent>;
-  let itemListService: ItemListService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        ItemListComponent,
-        ItemCreateComponent,
-        ItemCardComponent,
-        MarkdownPipe
-      ],
-      providers: [ItemListService, provideMockStore({})],
-      schemas: [NO_ERRORS_SCHEMA],
-      imports: [FormsModule, RouterTestingModule, StoreModule]
+      declarations: [ItemListComponent],
+      schemas: [NO_ERRORS_SCHEMA]
     }).compileComponents();
-
-    itemListService = TestBed.get(ItemListService);
   }));
 
   beforeEach(() => {
