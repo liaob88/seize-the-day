64b5eb22b454fd36837d0ab2e1b13513df5ed0b5
ryotaro-kitamura <k.ryoutaro.g@gmail.com>
2020-04-08 16:58:46 +0900

    firebase に deploy (deploy に伴う修正も含む)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCs4ZVhulRmUXBJotXlvfSFxHSBprJki1E',
+    authDomain: 'seize-the-day-e7257.firebaseapp.com',
+    databaseURL: 'https://seize-the-day-e7257.firebaseio.com',
+    projectId: 'seize-the-day-e7257',
+    storageBucket: 'seize-the-day-e7257.appspot.com',
+    messagingSenderId: '1087268439134',
+    appId: '1:1087268439134:web:7ef3cad4d29987923e60cf',
+    measurementId: 'G-MTT2GM8S79'
+  }
 };
