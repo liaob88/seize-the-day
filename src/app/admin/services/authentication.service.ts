525cb3dfb53779f3b7ba7ef0571f91a2aac93905
ryotaro-kitamura <k.ryoutaro.g@gmail.com>
2020-04-19 17:54:13 +0900

    login 機能

--- src/app/admin/services/authentication.service.ts
+++ src/app/admin/services/authentication.service.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth/';
 
@@ -5,16 +6,19 @@ import { AngularFireAuth } from '@angular/fire/auth/';
   providedIn: 'root'
 })
 export class AuthenticationService {
-  constructor(private auth: AngularFireAuth) {}
+  constructor(private auth: AngularFireAuth, private router: Router) {}
 
   signIn(email: string, password: string) {
-    this.auth.auth.signInWithEmailAndPassword(email, password).catch(error => {
-      alert('ログインできません。エラー内容は（' + error.message + '）です');
-    });
+    this.auth.auth
+      .signInWithEmailAndPassword(email, password)
+      .then(() => this.router.navigateByUrl('admin/dashboard'))
+      .catch(error => {
+        alert('ログインできません。エラー内容は（' + error.message + '）です');
+      });
   }
 
   signOut() {
-    this.auth.auth.signOut();
+    this.auth.auth.signOut().then(() => this.router.navigateByUrl('/'));
   }
 
   isAuthenticated(): boolean {
