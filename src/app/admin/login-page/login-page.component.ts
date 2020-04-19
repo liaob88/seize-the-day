bd43d4e14099b354c4bf7194eef87414c2b6b5d1
ryotaro-kitamura <k.ryoutaro.g@gmail.com>
2020-04-19 12:54:22 +0900

    loginComponent の作成

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthenticationService } from '../services/authentication.service';
 
 @Component({
   selector: 'app-login-page',
@@ -6,7 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-page.component.scss']
 })
 export class LoginPageComponent implements OnInit {
-  constructor() {}
+  constructor(
+    private auth: AuthenticationService,
+    private fb: FormBuilder
+  ) {}
+
+  loginForm: FormGroup = this.fb.group({
+    email: this.fb.control('', [Validators.required, Validators.email]),
+    password: this.fb.control('', [Validators.required])
+  });
 
   ngOnInit() {}
+
+  login() {
+    const { email, password } = this.loginForm.value;
+    this.auth.signIn(email, password);
+  }
 }
