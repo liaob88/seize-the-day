a1c52fd9fdee17fbc147c550de6c7d4438c26589
ryotaro-kitamura <k.ryoutaro.g@gmail.com>
2020-04-20 23:58:01 +0900

    エラーが回避のため BrowserAnimationsModule を追加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireStorageModule } from '@angular/fire/storage';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MarkdownModule } from 'ngx-markdown';
 import { environment } from './../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
@@ -16,6 +17,7 @@ import { SharedModule } from './shared/shared.module';
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     HomeModule,
     SharedModule,
